0687614eff5848e4515db8c2b96cbee96bbb9da5
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-03-12 17:43:34 +0700

    :sparkles: Show card's vote count in result page

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -2,17 +2,27 @@ import React from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
-const Component = ({ data: { loading, hello } }) => (
+const Card = ({ card }) => (
+  <div>
+    {card.value}: {card._votesMeta.count}
+  </div>
+)
+
+const Component = ({ data: { loading, allCards } }) => (
   <React.Fragment>
     <h1>Result</h1>
-    <div>{loading ? 'loading...' : hello.message}</div>
+    <div>{loading ? 'loading...' : allCards.map(card => <Card key={card.id} card={card} />)}</div>
   </React.Fragment>
 )
 
 const withQuery = graphql(gql`
   {
-    hello {
-      message
+    allCards {
+      id
+      value
+      _votesMeta {
+        count
+      }
     }
   }
 `)
